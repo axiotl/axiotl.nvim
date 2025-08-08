451e417aa68c458324222d9813d94faf08454ff9
Quin Scacheri <qs366@nyu.edu>
2025-08-08 13:15:20 -0400

    tree-sitter sync install

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,18 +1,18 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local configs = require("nvim-treesitter.configs")
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      local configs = require 'nvim-treesitter.configs'
 
-			configs.setup({
-				ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-				sync_install = false,
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-		end,
-	},
+      configs.setup {
+        ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'elixir', 'heex', 'javascript', 'html' },
+        sync_install = true, -- lets you run bootstrap.sh ahead of time to pre-download tree sitter parsers
+        highlight = { enable = true },
+        indent = { enable = true },
+      }
+    end,
+  },
 }
 -- return {
 -- 	{
